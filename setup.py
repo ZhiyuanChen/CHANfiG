821f8bdca25cb9f2a592c29d24b9306ecb71793a
Zhiyuan Chen <v-zhichen@microsoft.com>
2022-05-04 10:31:35 +0000

    add setup.py
    Signed-off-by: Zhiyuan Chen <v-zhichen@microsoft.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='chanfig',
+    version='0.0',
+    description='Easy Configuration',
+    author='Zhiyuan Chen',
+    author_email='this@zyc.ai',
+    url='http://github.com/ZhiyuanChen/chanfig',
+    packages=find_packages(),
+)
