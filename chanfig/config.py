043ab0e8f1becda633486dc11a34fa1745b376ae
Zhiyuan Chen <v-zhichen@microsoft.com>
2022-05-04 12:38:51 +0000

    add get and set ops
    Signed-off-by: Zhiyuan Chen <v-zhichen@microsoft.com>

--- chanfig/config.py
+++ chanfig/config.py
@@ -1,6 +1,9 @@
 from argparse import Namespace
 from ast import literal_eval
 from collections import OrderedDict
+from json import dump, dumps
+from os import PathLike
+from typing import Any
 
 
 class Config(Namespace):
@@ -8,19 +11,41 @@ class Config(Namespace):
     Basic Config
     """
 
-    def __setattr__(self, name, value) -> None:
-        try:
-            value = literal_eval(value)
-        except (ValueError, SyntaxError):
-            pass
-        if '.' in name:
-            name = name.split('.')
-            name, rest = name[0], '.'.join(name[1:])
+    delimiter: str = '.'
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        if self.delimiter in name:
+            name, rest = name.split(self.delimiter, 1)
             if not hasattr(self, name):
                 setattr(self, name, type(self)())
             setattr(getattr(self, name), rest, value)
+        elif isinstance(value, dict):
+            setattr(self, name, type(self)(**value))
         else:
+            if isinstance(value, str):
+                try:
+                    value = literal_eval(value)
+                except (ValueError, SyntaxError):
+                    pass
             super().__setattr__(name, value)
+
+    def __getattr__(self, name: str) -> Any:
+        if self.delimiter in name:
+            name, rest = name.split(self.delimiter, 1)
+            return getattr(self[name], rest)
+        else:
+            return super().__getattribute__(name)
+
+    __setitem__ = __setattr__
+    __getitem__ = __getattr__
+
+    def __contains__(self, name: str) -> bool:
+        return hasattr(self, name)
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        return self.dict() == other.dict()
 
     def dict(self) -> OrderedDict:
         dict = OrderedDict()
@@ -30,3 +55,12 @@ class Config(Namespace):
             else:
                 dict[k] = v
         return dict
+
+    def dump(self, fp):
+        if isinstance(fp, (str, bytes, PathLike)):
+            fp = open(fp)
+        dump(self.dict(), fp)
+        fp.close()
+
+    def dumps(self) -> str:
+        return dumps(self.dict())
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='chanfig',
-    version='0.0',
+    version='0.0.1',
     description='Easy Configuration',
     author='Zhiyuan Chen',
     author_email='this@zyc.ai',
