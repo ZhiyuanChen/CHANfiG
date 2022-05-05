9aefb54707cb46fc1c00da844593b813f6322a03
Zhiyuan Chen <v-zhichen@microsoft.com>
2022-05-05 03:57:07 +0000

    add yaml and json support
    Signed-off-by: Zhiyuan Chen <v-zhichen@microsoft.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='chanfig',
-    version='0.0.1',
+    version='0.0.2post1',
     description='Easy Configuration',
     author='Zhiyuan Chen',
     author_email='this@zyc.ai',
