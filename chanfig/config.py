42adaf71f2a7a24a51f2ef16cdff108a067d8fb8
Zhiyuan Chen <v-zhichen@microsoft.com>
2022-05-05 03:57:07 +0000

    add yaml and json support
    Signed-off-by: Zhiyuan Chen <v-zhichen@microsoft.com>

--- chanfig/config.py
+++ chanfig/config.py
@@ -1,9 +1,34 @@
+from __future__ import annotations
+
 from argparse import Namespace
 from ast import literal_eval
 from collections import OrderedDict
-from json import dump, dumps
-from os import PathLike
-from typing import Any
+from contextlib import contextmanager
+from json import dump as json_dump
+from json import dumps as json_dumps
+from json import load as json_load
+from os import PathLike as _PathLike
+from typing import IO, Any, Callable, Iterable, MutableMapping, Union
+
+from yaml import SafeDumper, SafeLoader
+from yaml import dump as yaml_dump
+from yaml import load as yaml_load
+
+PathLike = Union[str, _PathLike]
+File = Union[PathLike, IO]
+
+YAML_EXTENSIONS = ('.yml', '.yaml')
+JSON_EXTENSIONS = ('.json')
+PYTHON_EXTENSIONS = ('.py')
+
+
+class Dumper(SafeDumper):
+    def increase_indent(self, flow=False, indentless=False):
+        return super().increase_indent(flow, False)
+
+
+class FileExtensionError(ValueError):
+    pass
 
 
 class Config(Namespace):
@@ -12,6 +37,7 @@ class Config(Namespace):
     """
 
     delimiter: str = '.'
+    indent: int = 2
 
     def __setattr__(self, name: str, value: Any) -> None:
         if self.delimiter in name:
@@ -39,28 +65,78 @@ class Config(Namespace):
     __setitem__ = __setattr__
     __getitem__ = __getattr__
 
+    def __len__(self) -> int:
+        return len(self.__dict__)
+
+    def __iter__(self) -> Iterable:
+        return iter(self.__dict__)
+
     def __contains__(self, name: str) -> bool:
         return hasattr(self, name)
 
-    def __eq__(self, other) -> bool:
+    def __eq__(self, other: Config) -> bool:
         if not isinstance(other, type(self)):
             return NotImplemented
         return self.dict() == other.dict()
 
-    def dict(self) -> OrderedDict:
-        dict = OrderedDict()
+    def __str__(self) -> str:
+        return self.yamls()
+
+    def dict(self, cls: Callable = OrderedDict) -> MutableMapping:
+        dict = cls()
         for k, v in self.__dict__.items():
             if isinstance(v, Config):
-                dict[k] = v.dict()
+                dict[k] = v.dict(cls)
             else:
                 dict[k] = v
         return dict
 
-    def dump(self, fp):
-        if isinstance(fp, (str, bytes, PathLike)):
-            fp = open(fp)
-        dump(self.dict(), fp)
-        fp.close()
+    def json(self, file: File, *args, **kwargs):
+        if 'indent' not in kwargs:
+            kwargs['indent'] = self.indent
+        with self.open(file, mode='w') as fp:
+            json_dump(self.dict(), fp, *args, **kwargs)
 
-    def dumps(self) -> str:
-        return dumps(self.dict())
+    def jsons(self, *args, **kwargs):
+        if 'indent' not in kwargs:
+            kwargs['indent'] = self.indent
+        return json_dumps(self.dict(), *args, **kwargs)
+
+    def yaml(self, file: File, *args, **kwargs):
+        with self.open(file, mode='w') as fp:
+            self.yamls(fp, *args, **kwargs)
+
+    def yamls(self, *args, **kwargs):
+        if 'Dumper' not in kwargs:
+            kwargs['Dumper'] = Dumper
+        if 'indent' not in kwargs:
+            kwargs['indent'] = self.indent
+        return yaml_dump(self.dict(dict), *args, **kwargs)
+
+    @classmethod
+    def load(cls, path: str, **kwargs) -> Config:
+        path = path.lower()
+        with cls.open(path) as fp:
+            if path.endswith(JSON_EXTENSIONS):
+                config = cls(**json_load(fp, **kwargs))
+            elif path.endswith(YAML_EXTENSIONS):
+                if 'Loader' not in kwargs:
+                    kwargs['Loader'] = SafeLoader
+                config = cls(**yaml_load(fp.read(), **kwargs))
+            else:
+                raise FileExtensionError(f"path {path} should have extensions {JSON_EXTENSIONS} or {YAML_EXTENSIONS}")
+        return config
+
+    @staticmethod
+    @contextmanager
+    def open(file: File, *args, **kwargs):
+        if isinstance(file, (str, _PathLike )):
+            file = open(file, *args, **kwargs)
+            try:
+                yield file
+            finally:
+                file.close()
+        elif isinstance(file, (IO, )):
+            yield file
+        else:
+            raise ValueError(f"file {file} should be of type (str, os.PathLike) or (io.IOBase), but got {type(file)}")
