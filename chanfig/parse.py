45f036e09bd2c6ecb1ddb785d1f1cff9289024d8
Zhiyuan Chen <v-zhichen@microsoft.com>
2022-05-06 06:33:20 +0000

    add merge_from_file
    Signed-off-by: Zhiyuan Chen <v-zhichen@microsoft.com>

--- chanfig/parse.py
+++ chanfig/parse.py
@@ -14,10 +14,10 @@ class ConfigParser(ArgumentParser):
                 self.add_argument(arg)
         if config is None:
             config = Config()
-        if (path := getattr(config, config_name)) is not None:
+        if (path := getattr(config, config_name, None)) is not None:
             raise ValueError(f"{config} is reserved for auto loading config file, but got {path}")
         config, _ = self.parse_known_args(args, config)
-        if (path := getattr(config, config_name)) is not None:
+        if (path := getattr(config, config_name, None)) is not None:
             # CLI arguments have higher priority than config file
             other = Config.read(path)
             config = other.update(config)
