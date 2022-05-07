758b02f160070382c5ee7293b1f7333712ec144b
Zhiyuan Chen <v-zhichen@microsoft.com>
2022-05-07 15:25:10 +0000

    add freeze and defrost
    Signed-off-by: Zhiyuan Chen <v-zhichen@microsoft.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='chanfig',
-    version='0.0.5',
+    version='0.0.5post1',
     description='Easy Configuration',
     author='Zhiyuan Chen',
     author_email='this@zyc.ai',
