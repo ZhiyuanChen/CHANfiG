f41269dceb559f3a332e3dcde60d5efec1eb7ca8
Zhiyuan Chen <v-zhichen@microsoft.com>
2022-05-16 11:25:09 +0000

    fix update, diff and intersect not working for str input
    Signed-off-by: Zhiyuan Chen <v-zhichen@microsoft.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='chanfig',
-    version='0.0.9post4',
+    version='0.0.9post5',
     description='Easy Configuration',
     author='Zhiyuan Chen',
     author_email='this@zyc.ai',
