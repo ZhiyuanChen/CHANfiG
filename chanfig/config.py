e715e7aa6b6c9864038cc1efa93cad12bba79c15
Zhiyuan Chen <chenzhiyuan@dp.tech>
2022-11-04 12:13:20 +0800

    add inplace and setter for Variable

--- chanfig/config.py
+++ chanfig/config.py
@@ -101,6 +101,27 @@ class ConfigParser(ArgumentParser):  # pylint: disable=C0115
 class Variable:
     r"""
     Mutable wrapper for immutable objects.
+
+    Example:
+    ```python
+    >>> v = Variable(1)
+    >>> n = v
+    >>> v, n
+    (1, 1)
+    >>> v += 1
+    >>> v, n
+    (2, 2)
+    >>> v.value = 3
+    >>> v, n
+    (3, 3)
+    >>> v.set(4)
+    >>> v, n
+    (4, 4)
+    >>> v = 5
+    >>> v, n
+    (5, 4)
+
+    ```
     """
 
     storage: List[Any]
@@ -108,26 +129,52 @@ class Variable:
     def __init__(self, value):
         self.storage = [value]
 
+    @property
+    def value(self):
+        """
+        actual object stored in the Variable
+        """
+        return self.storage[0]
+
+    @value.setter
+    def value(self, value):
+        """
+        assign value to object stored in the Variable
+        """
+        self.storage[0] = value
+
+    def set(self, value):
+        """
+        alias of value.setter
+        """
+        self.value = value
+
+    @staticmethod
+    def _get_value(obj):
+        if isinstance(obj, Variable):
+            return obj.value
+        return obj
+
     def __getattr__(self, attr):
         return getattr(self.value, attr)
 
     def __lt__(self, other) -> bool:
-        return self.value < self._get(other)
+        return self.value < self._get_value(other)
 
     def __le__(self, other) -> bool:
-        return self.value <= self._get(other)
+        return self.value <= self._get_value(other)
 
     def __eq__(self, other) -> bool:
-        return self.value == self._get(other)
+        return self.value == self._get_value(other)
 
     def __ne__(self, other) -> bool:
-        return self.value != self._get(other)
+        return self.value != self._get_value(other)
 
     def __ge__(self, other) -> bool:
-        return self.value >= self._get(other)
+        return self.value >= self._get_value(other)
 
     def __gt__(self, other) -> bool:
-        return self.value > self._get(other)
+        return self.value > self._get_value(other)
 
     def __index__(self) -> int:
         return int(self.value)
@@ -139,58 +186,94 @@ class Variable:
         return abs(self.value)
 
     def __add__(self, other):
-        return Variable(self.value + self._get(other))
+        return Variable(self.value + self._get_value(other))
 
     def __radd__(self, other):
-        return Variable(self._get(other) + self.value)
+        return Variable(self._get_value(other) + self.value)
+
+    def __iadd__(self, other):
+        self.value += self._get_value(other)
+        return self
 
     def __and__(self, other):
-        return Variable(self.value & self._get(other))
+        return Variable(self.value & self._get_value(other))
 
     def __rand__(self, other):
-        return Variable(self._get(other) & self.value)
+        return Variable(self._get_value(other) & self.value)
+
+    def __iand__(self, other):
+        self.value &= self._get_value(other)
+        return self
 
     def __floordiv__(self, other):
-        return Variable(self.value // self._get(other))
+        return Variable(self.value // self._get_value(other))
 
     def __rfloordiv__(self, other):
-        return Variable(self._get(other) // self.value)
+        return Variable(self._get_value(other) // self.value)
+
+    def __ifloordiv__(self, other):
+        self.value //= self._get_value(other)
+        return self
 
     def __mod__(self, other):
-        return Variable(self.value % self._get(other))
+        return Variable(self.value % self._get_value(other))
 
     def __rmod__(self, other):
-        return Variable(self._get(other) % self.value)
+        return Variable(self._get_value(other) % self.value)
+
+    def __imod__(self, other):
+        self.value %= self._get_value(other)
+        return self
 
     def __mul__(self, other):
-        return Variable(self.value * self._get(other))
+        return Variable(self.value * self._get_value(other))
 
     def __rmul__(self, other):
-        return Variable(self._get(other) * self.value)
+        return Variable(self._get_value(other) * self.value)
+
+    def __imul__(self, other):
+        self.value *= self._get_value(other)
+        return self
 
     def __matmul__(self, other):
-        return Variable(self.value @ self._get(other))
+        return Variable(self.value @ self._get_value(other))
 
     def __rmatmul__(self, other):
-        return Variable(self._get(other) @ self.value)
+        return Variable(self._get_value(other) @ self.value)
+
+    def __imatmul__(self, other):
+        self.value @= self._get_value(other)
+        return self
 
     def __pow__(self, other):
-        return Variable(self.value ** self._get(other))
+        return Variable(self.value ** self._get_value(other))
 
     def __rpow__(self, other):
-        return Variable(self._get(other) ** self.value)
+        return Variable(self._get_value(other) ** self.value)
+
+    def __ipow__(self, other):
+        self.value **= self._get_value(other)
+        return self
 
     def __truediv__(self, other):
-        return Variable(self.value / self._get(other))
+        return Variable(self.value / self._get_value(other))
 
     def __rtruediv__(self, other):
-        return Variable(self._get(other) / self.value)
+        return Variable(self._get_value(other) / self.value)
+
+    def __itruediv__(self, other):
+        self.value /= self._get_value(other)
+        return self
 
     def __sub__(self, other):
-        return Variable(self.value - self._get(other))
+        return Variable(self.value - self._get_value(other))
 
     def __rsub__(self, other):
-        return Variable(self._get(other) - self.value)
+        return Variable(self._get_value(other) - self.value)
+
+    def __isub__(self, other):
+        self.value -= self._get_value(other)
+        return self
 
     def __copy__(self):
         return Variable(self.value)
@@ -268,19 +351,6 @@ class Variable:
         """
 
         return self.to(str)
-
-    @property
-    def value(self):
-        """
-        actual object stored in the Variable
-        """
-        return self.storage[0]
-
-    @staticmethod
-    def _get(obj):
-        if isinstance(obj, Variable):
-            return obj.value
-        return obj
 
 
 class OrderedDict(OrderedDict_):
