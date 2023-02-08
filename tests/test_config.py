f387bdf032284eeb53ef226d5c924b0488e53755
Zhiyuan Chen <this@zyc.ai>
2023-02-08 13:11:45 +0800

    fix value in config file overrided by default
    Signed-off-by: Zhiyuan Chen <this@zyc.ai>

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,7 +5,7 @@ class TestConfig(Config):
     __test__ = False
 
     def __init__(self, *args, **kwargs):
-        super().__init__()
+        super().__init__(*args, **kwargs)
         num_classes = Variable(10)
         self.name = "CHANfiG"
         self.seed = Variable(1013)
@@ -31,3 +31,13 @@ class Test:
 
     def test_fstring(self):
         assert f"seed{self.config.seed}" == "seed1013"
+
+    def test_load(self):
+        self.config.name = "Test"
+        self.config.dataset.num_classes = 12
+        self.config.dump("tests/test_config.json")
+        self.config = self.config.load("tests/test_config.json")
+        assert self.config.name == "Test"
+        assert self.config.network.name == "ResNet18"
+        assert self.config.network.num_classes == 12
+        assert self.config.dataset.num_classes == 12
