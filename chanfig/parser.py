04398c87725b316451a58342dcc14d7fe194c13f
Zhiyuan Chen <this@zyc.ai>
2023-07-17 16:45:42 +0800

    add copyright notice
    Signed-off-by: Zhiyuan Chen <this@zyc.ai>

--- chanfig/parser.py
+++ chanfig/parser.py
@@ -1,3 +1,19 @@
+# CHANfiG, Easier Configuration.
+# Copyright (c) 2022-2023, CHANfiG Contributors
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the following licenses:
+# - Unlicense
+# - GNU GPL 2.0 (or any later version)
+# - MIT
+# - Apache 2.0
+# - BSD 2-Clause
+# - BSD 3-Clause
+# - BSD 4-Clause
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the LICENSE file for more details.
+
 from __future__ import annotations
 
 import sys
