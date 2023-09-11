e0fdc21e6544cfdacc946b8e8e80e401895ed226
Zhiyuan Chen <this@zyc.ai>
2023-09-11 09:13:01 +0000

    improve merge
    Signed-off-by: Zhiyuan Chen <this@zyc.ai>

--- tests/test_nested_dict.py
+++ tests/test_nested_dict.py
@@ -52,17 +52,19 @@ class Test:
             "a": {"b": {"c": {"d": 3, "e": {"f": 4}}}}
         }
         d = NestedDict({"a": 1, "b.c": 2, "b.d": 3, "c.d.e": 4, "c.d.f": 5, "c.e": 6})
-        n = {"b": {"c": 3, "d": 5}, "c.d.e": 4, "c.d": {"f": 5}, "d": 0}
-        assert d.merge(n).dict() == {"a": 1, "b": {"c": 3, "d": 5}, "c": {"d": {"e": 4, "f": 5}, "e": 6}, "d": 0}
+        m = {"b": {"c": 3, "d": 5}, "c.d.e": 4, "c.d": {"f": 5}, "d": 0}
+        assert d.merge(m).dict() == {"a": 1, "b": {"c": 3, "d": 5}, "c": {"d": {"e": 4, "f": 5}, "e": 6}, "d": 0}
         assert NestedDict(a=1, b=1, c=1).union(NestedDict(b="b", c="c", d="d")).dict() == {
             "a": 1,
             "b": "b",
             "c": "c",
             "d": "d",
         }
+        n = {"b": 2, "c.d.e": 4, "c.d": {"f": 5}, "d": 0}
+        assert d.merge(n).dict() == {"a": 1, "b": 2, "c": {"d": {"e": 4, "f": 5}, "e": 6}, "d": 0}
         d = NestedDict()
         d.c = {"b": {"d": 3, "e": {"f": 4}}}
-        assert d.merge(n).dict() == {"c": {"b": {"d": 3, "e": {"f": 4}}, "d": {"f": 5}}, "b": {"c": 3, "d": 5}, "d": 0}
+        assert d.merge(m).dict() == {"c": {"b": {"d": 3, "e": {"f": 4}}, "d": {"f": 5}}, "b": {"c": 3, "d": 5}, "d": 0}
         d = NestedDict()
         assert d.merge(a={1: 1}, b={2: 2}, c={3: 3}).dict() == {"a": {1: 1}, "b": {2: 2}, "c": {3: 3}}
         assert d.merge(d.clone()).dict() == {"a": {1: 1}, "b": {2: 2}, "c": {3: 3}}
