c930b789bbf9e5176217b8f793ebe31103a42bda
Zhiyuan Chen <this@zyc.ai>
2024-08-22 00:31:53 +0800

    add tests
    Signed-off-by: Zhiyuan Chen <this@zyc.ai>

---
+++ tests/test_registry.py
@@ -0,0 +1,30 @@
+# CHANfiG, Easier Configuration.
+# Copyright (c) 2022-Present, CHANfiG Contributors
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the following licenses:
+# - The Unlicense
+# - GNU Affero General Public License v3.0 or later
+# - GNU General Public License v2.0 or later
+# - BSD 4-Clause "Original" or "Old" License
+# - MIT License
+# - Apache License 2.0
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the LICENSE file for more details.
+
+
+from chanfig import ConfigRegistry as Registry_
+
+
+class Registry(Registry_):
+    key = "level"
+
+
+class TestRegistry:
+
+    def test_registry(self):
+        registry = Registry()
+        assert registry.getattr("key") == "level"
